7a94302974d5df7b45720ebc3fef154eeab51b72
ilyasse el haddad <68947535+ilyasse406@users.noreply.github.com>
2021-01-17 11:56:54 +0100

    wolf-hackathon

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "June 04, 2021 18:24:33",
+  let birthday = "June 01, 2021 00:00:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
 
